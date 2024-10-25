11ed907b5cf547b1ce507b1cc56469d935574a17
Samuel Huang <56812343+upieez@users.noreply.github.com>
2024-10-25 21:46:30 +0800

    add router dev dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [TanStackRouterVite(), react()],
+});
