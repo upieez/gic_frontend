4622865db3be59524ce3f48f085c325bfc8f37a2
Samuel Huang <56812343+upieez@users.noreply.github.com>
2024-10-27 14:28:08 +0800

    install react form

--- src/routes/cafes/create.tsx
+++ src/routes/cafes/create.tsx
@@ -1,5 +1,31 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { Button, FormControl, Stack, TextField } from "@mui/material";
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/cafes/create')({
-  component: () => <div>Hello /cafes/create!</div>,
-})
+export const Route = createFileRoute("/cafes/create")({
+  component: CreateCafe,
+});
+
+function CreateCafe() {
+  return (
+    <form>
+      <FormControl>
+        <TextField id="name" label="Name" variant="standard" required />
+        <TextField
+          id="description"
+          label="Description"
+          variant="standard"
+          required
+        />
+        <TextField id="location" label="Location" variant="standard" required />
+        <Stack direction="row" spacing={2}>
+          <Button variant="outlined" color="primary">
+            Cancel
+          </Button>
+          <Button variant="contained" color="primary">
+            Submit
+          </Button>
+        </Stack>
+      </FormControl>
+    </form>
+  );
+}
