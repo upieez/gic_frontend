288859c476b676e0f129dedf2e3756051a53f1ad
Samuel Huang <56812343+upieez@users.noreply.github.com>
2024-10-28 12:43:14 +0800

    cafe to use type

--- src/hooks/useCreateCafe.tsx
+++ src/hooks/useCreateCafe.tsx
@@ -1,14 +1,11 @@
 import { useMutation } from "@tanstack/react-query";
 import axios from "redaxios";
 import { API_URL } from "../constants";
+import { ICafeForm } from "../components/CafeForm";
 
 export function useCreateCafe() {
   return useMutation({
-    mutationFn: (data: {
-      name: string;
-      description: string;
-      location: string;
-    }) => {
+    mutationFn: (data: ICafeForm) => {
       return axios.post(`${API_URL}/cafe`, data);
     },
   });
--- src/hooks/useEditCafe.tsx
+++ src/hooks/useEditCafe.tsx
@@ -1,15 +1,11 @@
 import { useMutation } from "@tanstack/react-query";
 import axios from "redaxios";
 import { API_URL } from "../constants";
+import { ICafeForm } from "../components/CafeForm";
 
 export function useEditCafe() {
   return useMutation({
-    mutationFn: (data: {
-      id: string;
-      name: string;
-      description: string;
-      location: string;
-    }) => {
+    mutationFn: (data: ICafeForm & { id: string }) => {
       return axios.put(`${API_URL}/cafe/${data.id}`, data);
     },
   });
